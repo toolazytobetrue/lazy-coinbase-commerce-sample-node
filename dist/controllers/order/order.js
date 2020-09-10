875e579c16500701dabaff14730159d0787a39e6
Hassan Jawhar <hassan.jawhar@gotocme.com>
2020-09-10 14:04:25 +0300

    updated

--- dist/controllers/order/order.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createOrder = void 0;
-const order_create_1 = require("./order-create");
-Object.defineProperty(exports, "createOrder", { enumerable: true, get: function () { return order_create_1.createOrder; } });
-//# sourceMappingURL=order.js.map
